0a5f2c69575b94a282815dfccbe7c5a640451aba
Sjoerd Braaksma <sbr162@uwv.nl>
2024-07-29 13:18:50 +0000

    new init for azureml

--- {{cookiecutter.project_slug}}/init_env.sh
+++ {{cookiecutter.project_slug}}/init_env.sh
@@ -1,7 +1,7 @@
 #  Ensure in base environment of conda
 # Install all these
 echo "installing pre-commit"
-command source activate base
+command conda activate base
 command pip3 install pre-commit
 
 # Install requirements.txt if it's there
@@ -10,8 +10,8 @@ if [ -f requirements.txt ]; then
 fi
 
 # Install & activate conda environment 
-conda_env=$(yq -r .name conda_env.yaml)
-if if conda env list | grep -q "\b$conda_env\b"; then
+conda_env="{{cookiecutter.project_slug}}"
+if conda env list | grep -q "\b$conda_env\b"; then
     echo "A conda environment with the same name already exists, skipping installation."
 else
     echo "Creating conda environment from YAML"
